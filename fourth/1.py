4fa7a894641d5de5f411d647b093328a1fcf2484
MagicWinnie <magicwinnie@yandex.ru>
2022-12-15 15:07:51 +0300

    1 task solved

--- fourth/1.py
+++ fourth/1.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict
 import re
 
 
@@ -36,28 +36,25 @@ def get_regex(nucl: str) -> str:
 
 
 def get_compliment(sequence: str) -> str:
-    map_nucl = {
+    map_nucl: Dict[str, str] = {
         'A': 'T',
         'T': 'A',
         'C': 'G',
         'G': 'C'
     }
-    compliment = ''.join([map_nucl[ch] for ch in sequence])
+    compliment: str = ''.join([map_nucl[ch] for ch in sequence])
     return compliment
 
 
 def find_target(seq: str, pam: str, orient_pam: str, length_target: int) -> List[str]:
-    pam_regex = ''.join(list(map(get_regex, pam)))
-    target_regex_r = '([AGCT]{' + str(length_target) + '})' + '(' + pam_regex + ')'
-    target_regex_l = '(' + pam_regex + ')' + '([AGCT]{' + str(length_target) + '})'
-    compliment = get_compliment(seq)
-    output_list = []
-    output_list += [x[0] if orient_pam == 'L' else x[0] for x in re.findall(target_regex_r, seq)]
-    output_list += [x[1] if orient_pam == 'L' else x[0] for x in re.findall(target_regex_l, seq[::-1])]
-    output_list += [x[0] if orient_pam == 'L' else x[0] for x in re.findall(target_regex_r, compliment)]
-    output_list += [x[1] if orient_pam == 'L' else x[0] for x in re.findall(target_regex_l, compliment[::-1])]
+    pam_regex: str = ''.join(list(map(get_regex, pam)))
+    if orient_pam == 'R':
+        target_regex: str = f'(?=([AGCT]{{{str(length_target)}}})({pam_regex}))'
+    else:
+        target_regex: str = f'(?=({pam_regex})([AGCT]{{{str(length_target)}}}))'
+    regex = re.compile(target_regex)
+    compliment: str = get_compliment(seq)
+    output_list: List[str] = []
+    output_list += [x[0] if orient_pam == 'R' else x[1] for x in regex.findall(seq)]
+    output_list += [x[0] if orient_pam == 'R' else x[1] for x in regex.findall(compliment[::-1])]
     return output_list
-
-seq, pam, or_pam, len_target = input().split()
-print(find_target(seq, pam, or_pam, int(len_target)))
-print(get_compliment('TAGCTACGATCGATCGTTTCTAGCTACGATGCAAGAAAGATCGATCGATCGACGTACG')[::-1])
